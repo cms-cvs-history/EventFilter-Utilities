0e89fcb9d32b88cc9f5c487af510313b0fc992db
cmsbuild <>
2007-06-06 21:49:27 +0000

    This commit was manufactured by cvs2git to create tag 'EventFilter- Utilities-jun21xdaq39'.
    Sprout from master 2007-06-06 21:49:23 UTC Emilio Meschi <sha1-0fd28efec68b15b9a22d76a58948bc83df2ae146@cern.ch> 'xdaq3.9 port first pass'
    Cherrypick from master 2007-03-08 17:14:30 UTC Philipp Schieferdecker <sha1-d9aae2aca46c6a9ac4cda95dc40c0d5c7fc4181d@cern.ch> 'tag V00-12-00: compliant with FWCore reorganization in 140 series; evf::StateMachine to be implemented by all FilterFarm apps, see TWIKI page':
        BuildFile
        src/SealModule.cc
    Cherrypick from EventFilter-Utilities-branch_1_4_X 2007-05-10 08:54:10 UTC Emilio Meschi <sha1-0fd28efec68b15b9a22d76a58948bc83df2ae146@cern.ch> 'fix synchronization problems in MicroStateService':
        ChangeLog
    Delete:
        doc/Utilities.doc
        plugins/BuildFile
        plugins/SealModule.cc

--- src/SealModule.cc
+++ src/SealModule.cc
@@ -1,3 +1,4 @@
+#include "PluginManager/ModuleDef.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
 #include "FWCore/ServiceRegistry/interface/ServiceMaker.h"
 #include "EventFilter/Utilities/interface/MicroStateService.h"
@@ -9,5 +10,6 @@ using namespace evf;
 typedef edm::serviceregistry::AllArgsMaker<MicroStateService> MicroStateServiceMaker;
 typedef ParameterSetMaker<ModuleWebRegistry> maker;
 
+DEFINE_SEAL_MODULE();
 DEFINE_ANOTHER_FWK_SERVICE_MAKER(MicroStateService, MicroStateServiceMaker);
 DEFINE_ANOTHER_FWK_SERVICE_MAKER(ModuleWebRegistry,maker);
